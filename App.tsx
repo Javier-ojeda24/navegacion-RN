61d0422e0e6d60378a7d0a6c07c10485baffde81
Javier-ojeda24 <richardjavierojeda2021@gmail.com>
2023-08-01 09:03:57 -0300

    insall stack

--- App.tsx
+++ App.tsx
@@ -1,11 +1,26 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {Text, View} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Notifications" component={Notifications} />
+      <Stack.Screen name="Profile" component={Profile} />
+      <Stack.Screen name="Settings" component={Settings} />
+    </Stack.Navigator>
+  );
+}
 
 const App = () => {
   return (
-    <View>
+    <Stack.Navigator>
       <Text>App screen</Text>
-    </View>
+    </Stack.Navigator>
   );
 };
 
